d2284e9aa1235648bb7a84eddf75d0a672a9a9cc
Jashan <dhimanjashan548@gmail.com>
2025-12-30 11:30:05 +0530

    import.meta.url with fileURLToPath

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoute.js";
 import { connectToMongo } from "./db.js";
 import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // MongoDB Connection
