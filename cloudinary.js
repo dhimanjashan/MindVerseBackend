022e9b284f2ffaefefa3956648ff5b33bfb652e0
Jashan <dhimanjashan548@gmail.com>
2026-01-09 14:53:20 +0530

    Correct the cloudinary.js file

--- cloudinary.js
+++ cloudinary.js
@@ -1,8 +1,9 @@
-import { vs as cloudinary } from "cloudinary";
+import cloudinary from "cloudinary";
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
-export default cloudinary;
+
+export default cloudinary.v2;
